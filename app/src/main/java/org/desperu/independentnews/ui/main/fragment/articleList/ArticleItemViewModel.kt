dec0d47a0dd7e0b5cf7f0b6ba7f30d187a011919
desperu <motais.esteban@laposte.net>
2020-12-27 17:59:34 +0100

    Add mark read articles with a specific color. Update basta background color.

--- app/src/main/java/org/desperu/independentnews/ui/main/fragment/articleList/ArticleItemViewModel.kt
+++ app/src/main/java/org/desperu/independentnews/ui/main/fragment/articleList/ArticleItemViewModel.kt
@@ -2,26 +2,34 @@ package org.desperu.independentnews.ui.main.fragment.articleList
 
 import android.view.View
 import android.view.View.OnClickListener
+import androidx.databinding.ObservableBoolean
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.desperu.independentnews.R
 import org.desperu.independentnews.models.Article
+import org.desperu.independentnews.repositories.IndependentNewsRepository
 import org.koin.core.KoinComponent
 import org.koin.core.get
 
 /**
  * View Model witch provide data for article item.
  *
- * @property article    the given article data for this view model.
- * @property router     the router that allows redirection of the user.
+ * @property article            the given article data for this view model.
+ * @property ideNewsRepository  the app repository interface witch provide database and network access.
+ * @property router             the router that allows redirection of the user.
  *
  * @constructor Instantiates a new ArticleItemViewModel.
  *
- * @param article       the given article data for this view model to set.
+ * @param article               the given article data for this view model to set.
  */
 class ArticleItemViewModel(val article: Article): ViewModel(), KoinComponent {
 
     // FOR DATA
+    private val ideNewsRepository: IndependentNewsRepository = get()
     private val router: ArticleRouter get() = get()
+    val isRead = ObservableBoolean(article.read)
 
     // ------------
     // LISTENERS
@@ -31,6 +39,7 @@ class ArticleItemViewModel(val article: Article): ViewModel(), KoinComponent {
      * On click image listener.
      */
     val onClickImage = OnClickListener {
+        markAsRead()
         router.openShowArticle(article, it)
     }
 
@@ -38,6 +47,7 @@ class ArticleItemViewModel(val article: Article): ViewModel(), KoinComponent {
      * On click title listener.
      */
     val onClickTitle = OnClickListener {
+        markAsRead()
         router.openShowArticle(article, (it.parent as View).findViewById(R.id.image))
     }
 
@@ -45,6 +55,19 @@ class ArticleItemViewModel(val article: Article): ViewModel(), KoinComponent {
      * On click description container listener.
      */
     val onClickDescriptionContainer = OnClickListener {
+        markAsRead()
         router.openShowArticle(article, (it.parent as View).findViewById(R.id.image))
+    }
+
+    // ------------
+    // UTILS
+    // ------------
+
+    /**
+     * Mark the article as read in current list and database.
+     */
+    private fun markAsRead() = viewModelScope.launch(Dispatchers.IO) {
+        isRead.set(true)
+        ideNewsRepository.markArticleAsRead(article)
     }
 }
